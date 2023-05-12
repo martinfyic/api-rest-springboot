c182ba70261f7d1c592c98a5bfd1c17184ed2ebb
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-12 14:08:38 -0300

    change logic with lombok

--- src/main/java/com/javaapirest/apirest/models/Usuario.java
+++ src/main/java/com/javaapirest/apirest/models/Usuario.java
@@ -1,97 +1,36 @@
 package com.javaapirest.apirest.models;
 
-import java.time.LocalDate;
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@Table(name = "usuarios")
+@ToString @EqualsAndHashCode
 public class Usuario {
 
-    private final UUID idUsuario;
+    @Getter @Setter @Column(name = "id")
+    @Id
+    private String idUsuario;
+
+    @Getter @Setter @Column(name = "nombre")
     private String nombre;
+
+    @Getter @Setter @Column(name = "apellido")
     private String apellido;
+
+    @Getter @Setter @Column(name = "email")
     private String email;
+
+    @Getter @Setter @Column(name = "telefono")
     private String telefono;
+
+    @Getter @Setter @Column(name = "password")
     private String password;
-    private final LocalDate fecha_creado;
 
-    public Usuario(String nombre, String apellido, String email, String telefono, String password) {
-
-        this.idUsuario = UUID.randomUUID();
-        setNombre(nombre);
-        setApellido(apellido);
-        setEmail(email);
-        setTelefono(telefono);
-        setPassword(password);
-        this.fecha_creado = LocalDate.now();
-    }
-
-    public UUID getIdUsuario() {
-        return idUsuario;
-    }
-
-    public LocalDate getFecha_creado() {
-        return fecha_creado;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        if (nombre != null) {
-            this.nombre = nombre;
-        }
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        if (apellido != null) {
-            this.apellido = apellido;
-        }
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        if (email != null) {
-            this.email = email;
-        }
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        if (telefono != null) {
-            this.telefono = telefono;
-        }
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        if (password != null) {
-            this.password = password;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "idUsuario=" + idUsuario +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", email='" + email + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", password='" + password + '\'' +
-                ", fecha_creado=" + fecha_creado +
-                '}';
-    }
 }
