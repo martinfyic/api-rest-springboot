e2e54c8d1895c5f6fc8951c12d985141ad38d57b
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-12 19:28:49 -0300

    connect db with endpoint GET & DELETE
    detail: GET:api/usuarios GET:api/usuarios/{id} & DELETE:api/usuarios/{id}

--- src/main/java/com/javaapirest/apirest/controllers/UsuarioController.java
+++ src/main/java/com/javaapirest/apirest/controllers/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.javaapirest.apirest.controllers;
 
+import com.javaapirest.apirest.dao.UsuarioDao;
 import com.javaapirest.apirest.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,25 +14,21 @@ import java.util.ArrayList;
 @RestController
 public class UsuarioController {
 
-    @GetMapping(value="/usuarios")
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @GetMapping(value="api/usuarios")
     public List<Usuario> getusuarios() {
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario user1 = new Usuario("Martin", "Ferreira", "work@gmail.com", "09098434343", "2222222");
-        Usuario user2 = new Usuario("Fernando", "Bueno", "work1@gmail.com", "4454543", "222r4rsdf2222");
-        Usuario user3 = new Usuario("Marcos", "Padilla", "work2@gmail.com", "0909866329434343", "fweer4y");
-        Usuario user4 = new Usuario("Federico", "Affonso", "work3@gmail.com", "0905984543", "Sdff");
-
-        usuarios.add(user1);
-        usuarios.add(user2);
-        usuarios.add(user3);
-        usuarios.add(user4);
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
-    @GetMapping(value="/usuario") // modificar URL para buscar por id {id}
-    public Usuario getusuario() {
-        return new Usuario("Martin", "Ferreira", "work@gmail.com", "09098434343", "2222222");
+    @GetMapping(value="api/usuarios/{id}")
+    public Usuario getUsuarioById(@PathVariable double id) {
+        return usuarioDao.getUsuarioById(id);
+    }
+
+    @DeleteMapping(value="api/usuarios/{id}")
+    public void deleteUsuario(@PathVariable double id) {
+        usuarioDao.deleteUsuario(id);
     }
 }
